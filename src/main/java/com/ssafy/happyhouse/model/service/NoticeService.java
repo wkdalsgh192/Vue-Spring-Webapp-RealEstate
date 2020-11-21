873ac2b018c9295f1017a5592ead481e9313e1e3
장민호 <paranpu@naver.com>
2020-11-21 04:11:35 +0000

    Deleted .mvn/wrapper/MavenWrapperDownloader.java, .mvn/wrapper/maven-wrapper.jar, .mvn/wrapper/maven-wrapper.properties, src/main/java/com/ssafy/happyhouse/controller/HouseMapController.java, src/main/java/com/ssafy/happyhouse/controller/MemberController.java, src/main/java/com/ssafy/happyhouse/controller/NoticeController.java, src/main/java/com/ssafy/happyhouse/controller/PageController.java, src/main/java/com/ssafy/happyhouse/model/mapper/HouseMapMapper.java, src/main/java/com/ssafy/happyhouse/model/mapper/MemberMapper.java, src/main/java/com/ssafy/happyhouse/model/mapper/NoticeMapper.java, src/main/java/com/ssafy/happyhouse/model/service/HouseMapService.java, src/main/java/com/ssafy/happyhouse/model/service/HouseMapServiceImpl.java, src/main/java/com/ssafy/happyhouse/model/service/MemberService.java, src/main/java/com/ssafy/happyhouse/model/service/MemberServiceImpl.java, src/main/java/com/ssafy/happyhouse/model/service/NoticeService.java, src/main/java/com/ssafy/happyhouse/model/service/NoticeServiceImpl.java, src/main/java/com/ssafy/happyhouse/model/GugunDto.java, src/main/java/com/ssafy/happyhouse/model/HouseDealDto.java, src/main/java/com/ssafy/happyhouse/model/HouseInfoDto.java, src/main/java/com/ssafy/happyhouse/model/MemberDto.java, src/main/java/com/ssafy/happyhouse/model/NoticeDto.java, src/main/java/com/ssafy/happyhouse/model/SidoDto.java, src/main/java/com/ssafy/happyhouse/model/SidoGugunCodeDto.java, src/main/java/com/ssafy/happyhouse/HappyhouseSpringbootApplication.java, src/main/resources/mapper/housemap.xml, src/main/resources/mapper/member.xml, src/main/resources/mapper/notice.xml, src/main/resources/sql/happyhouse_dump.sql, src/main/resources/sql/happyhouse_sync.sql, src/main/resources/static/forum/call-address.js, src/main/resources/static/forum/custom.css, src/main/resources/static/forum/custom.js, src/main/resources/static/forum/kakaomap.js, src/main/resources/static/global/scripts.js, src/main/resources/static/global/styles.css, src/main/resources/static/img/bg-masthead.jpg, src/main/resources/static/img/bg-signup.jpg, src/main/resources/static/img/demo-image-01.jpg, src/main/resources/static/img/demo-image-02.jpg, src/main/resources/static/img/dogok-palace.jpg, src/main/resources/static/img/dogok-raemian.jpg, src/main/resources/static/img/dogok-rexle.jpg, src/main/resources/static/img/favicon.ico, src/main/resources/static/img/ipad.png, src/main/resources/static/img/main.png, src/main/resources/static/img/주택1.png, src/main/resources/static/settings/settings.css, src/main/resources/application.properties, src/main/webapp/WEB-INF/views/project/aptDeal.jsp, src/main/webapp/WEB-INF/views/project/board.jsp, src/main/webapp/WEB-INF/views/project/dongDeal.jsp, src/main/webapp/WEB-INF/views/project/forum.html, src/main/webapp/WEB-INF/views/project/page1.html, src/main/webapp/WEB-INF/views/project/settings.jsp, src/main/webapp/WEB-INF/views/index.jsp, src/test/java/com/ssafy/haapyhouse/HappyhouseSpringbootApplicationTests.java, capture/map.png, capture/공지사항.png, capture/로그인.png, capture/회원가입.png, capture/회원정보 수정.png, capture/회원탈퇴.png, .springWebflow, HappyHouse_Web_Spring_구미_2반_송원준_장민호.zip, README.md, mvnw, mvnw.cmd, pom.xml, update(11.09).zip files

--- src/main/java/com/ssafy/happyhouse/model/service/NoticeService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.happyhouse.model.service;
-
-import java.util.List;
-
-import com.ssafy.happyhouse.model.NoticeDto;
-
-public interface NoticeService {
-	public List<NoticeDto> noticeList() throws Exception;
-}
